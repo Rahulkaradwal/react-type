764f8cb0543a1980d3dacd6a511870c7de843161
rahul karadwal <rahulkaradwal2000@gmail.com>
2024-11-10 14:10:33 -0500

    create new project for pizza menu

---
+++ pizza/src/App.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactElement } from "react";
+
+const App: FC = (): ReactElement => {
+  return (
+    <div className="bg-red-500">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
