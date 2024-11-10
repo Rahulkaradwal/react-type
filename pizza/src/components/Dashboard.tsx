1c1fd580f505d57b29c79d018c5576d8733414d0
rahul karadwal <rahulkaradwal2000@gmail.com>
2024-11-10 14:40:13 -0500

    add footer

--- pizza/src/components/Dashboard.tsx
+++ pizza/src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactElement } from "react";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 export const Dashboard: FC = (): ReactElement => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
