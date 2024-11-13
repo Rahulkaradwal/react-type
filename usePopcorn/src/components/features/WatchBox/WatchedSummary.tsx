ed7a3b7ccf8c211547f16162fa92e7f51c3b58ce
rahul karadwal <rahulkaradwal2000@gmail.com>
2024-11-13 16:56:54 -0500

    end of project

--- usePopcorn/src/components/features/WatchBox/WatchedSummary.tsx
+++ usePopcorn/src/components/features/WatchBox/WatchedSummary.tsx
@@ -1,14 +1,24 @@
 import { FC, ReactElement } from "react";
+import { IMovie } from "../../Types";
 
-export const WatchedSummary: FC = (): ReactElement => {
+interface WatchedSummaryProps {
+  watchedMovie: IMovie[];
+}
+
+export const WatchedSummary: FC<WatchedSummaryProps> = ({
+  watchedMovie,
+}): ReactElement => {
+  // const totalMovies = watchedMovie.length;
+  const totalMovies = 5;
+  const totalTime = 19;
   return (
     <div className="bg-gray-700 rounded-b-lg -mx-4 p-4 shadow-lg mb-4">
       <div>MOVIES YOU WATCHED</div>
       <div className="flex justify-between items-center ">
-        <p># 0 movies</p>
+        <p># {totalMovies} movies</p>
+        <p>{totalTime}.0</p>
         <p>0.0</p>
-        <p>0.0</p>
-        <p>0 min</p>
+        <p>{totalTime} min</p>
       </div>
     </div>
   );
