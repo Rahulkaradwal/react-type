922f6743244dec17cd2d18f73c320b7a5ee36b77
rahul karadwal <rahulkaradwal2000@gmail.com>
2024-11-17 14:25:14 -0500

    initialized worldwise project

---
+++ worldwise/src/App.tsx
@@ -0,0 +1,7 @@
+import { FC, ReactElement } from "react";
+
+const App: FC = (): ReactElement => {
+  return <h1 className="bg-red-500">hello</h1>;
+};
+
+export default App;
---
+++ worldwise/src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactElement } from "react";
+
+export const Homepage: FC = (): ReactElement => {
+  return (
+    <main>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
+  );
+};
